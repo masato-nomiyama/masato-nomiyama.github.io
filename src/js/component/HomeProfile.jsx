b6c56a474ba810a050e2e7c1819704bec94f1017
Masato Nomiyama <nomy@takram.com>
2020-02-12 01:10:58 +0900

    improve writing in the top page

--- src/js/component/HomeProfile.jsx
+++ src/js/component/HomeProfile.jsx
@@ -23,24 +23,28 @@ const renderCopywrite = props => {
   return (
     <div className={`${props.className}-item`}>
       <Typography variant='title' className={props.classes.profileTitle}>
-        感性 × エンジニアリング<br/><br/>
+        感性のエンジニアリング<br/><br/>
       </Typography>
       <Typography variant='body2' className={props.classes.profileBody}>
-        私たちの行動や暮らしは、<br/>
-        私たちが思う以上に感性に根ざしている。<br/>
-        <br/>
-        突き動かされるように想いを誰かに伝え、<br/>
-        旅の偶然の出会いに胸を打たれ、<br/>
-        日々の些細な変化に風情を感じる。<br/>
-        <br/>
-        もしひとの持つ感性をエンジニアリングできたなら、<br/>
-        ひとはもっと豊かに、創造的に暮らせるのではないだろうか。<br/>
-        <br/>
-        鍵となるのは心の形とその変化。<br/>
-        心に形を与えることで想いの機微に目を向け、<br/>
-        うつろう心を捉えることで小さな気づきに耳を澄ます。<br/>
-        <br/>
-        ひとに寄り添うエンジニアリングで、<br/>
+        私たちの暮らしは<br/>
+        私たちの持つ感性に根ざしている。<br/>
+      </Typography>
+      <Typography variant='body2' className={props.classes.profileBody}>
+        突き動かされるように誰かに想いを伝えたり<br/>
+        日々の些細な変化に風情を感じたり<br/>
+        偶然の出会いに胸を打たれたりする。<br/>
+      </Typography>
+      <Typography variant='body2' className={props.classes.profileBody}>
+        もしこうした感性をエンジニアリングできたら<br/>
+        ひとはもっと豊かに暮らせるのではないだろうか。<br/>
+      </Typography>
+      <Typography variant='body2' className={props.classes.profileBody}>
+        もし、伝えきれない心の機微まで分かち合えたら。<br/>
+        もし、忙しい日々のなかでも環境の変化を楽しめたら。<br/>
+        もし、感動的な体験を前もって計画できたら。<br/>
+      </Typography>
+      <Typography variant='body2' className={props.classes.profileBody}>
+        ひとに寄り添う「感性のエンジニアリング」で<br/>
         暮らしをもっと豊かに。<br/>
       </Typography>
     </div>
